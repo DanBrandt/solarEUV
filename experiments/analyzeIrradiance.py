solomon.py: Completed initial fix to computing irradiance in the Solomon bands; the issue was that the wrong columns were being taken from the Solomon Table before doing the calculation.

experiments/analyzeIrradiance.py
@@ -45,6 +45,18 @@ pylab.rcParams.update(params)
 #-----------------------------------------------------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------------------------------------
+# Helper Functions:
+def energy(watt):
+    """
+    Given the energy in watts, return the energy in joules, provided that the time given is in seconds.
+    """
+    return watt * 86400
+
+def power(myEnergy):
+    return myEnergy / 86400
+#-----------------------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------------------------
 # Execution:
 if __name__=="__main__":
     # ==================================================================================================================
@@ -56,6 +68,7 @@ if __name__=="__main__":
     times = toolbox.loadPickle(omniTimesData)
     times = np.array([element + timedelta(hours=12) for element in times])
     F107 = toolbox.loadPickle(omniF107Data)
+    F107A = toolbox.rollingAverage(F107, 81, True, True)
     F107B = toolbox.loadPickle(omniF107AveData)
 
     # Obtain FISM2 data:
@@ -90,6 +103,9 @@ if __name__=="__main__":
     np.where((myIrrTimesFISM2Bands >= times[0]) & (myIrrTimesFISM2Bands <= times[-1]))[0]
     correspondingIrrTimesFISM2StanBands = myIrrTimesFISM2Bands[correspondingIndsFISM2StanBands]
     correspondingIrrFISM2StanBands = myIrrDataAllFISM2BandsFixed[correspondingIndsFISM2StanBands, :]
+
+    # Determine how the raw FISM2 data are to be rebinned into the Solomon Bands:
+    # toolbox.solomonAnalysis(correspondingIrrFISM2StanBands, solomonTable, myIrrDataAllFISM2[correspondingIndsFISM2, :], wavelengthsFISM2)
 
     # Manually rebinning of raw FISM2 data to the Solomon bins (to check the binning procedure):
     myIrrDataWavelengthsFISM2_Solomon_n, rebinnedIrrDataFISM2_Solomon_n = toolbox.newbins(wavelengthsFISM2, myIrrDataAllFISM2,
@@ -132,7 +148,7 @@ if __name__=="__main__":
 
     # ==================================================================================================================
     # 2: Run the empirical models:
-    override = False # If True, recompute everything
+    override = True # If True, recompute everything
     if os.path.isfile(results_dir+'cachedData.pkl') == True and override == False:
         cached=True
         cachedData = toolbox.loadPickle(results_dir+'cachedData.pkl')
@@ -173,10 +189,10 @@ if __name__=="__main__":
                                                                           statsFiles=['corMat.pkl', 'sigma_NEUVAC.pkl'])
 
         # Generate EUVAC data:
-        # ensemble_EuvacIrr, ensemble_average_EuvacIrr, ensemble_stddev_EuvacIrr = irradiance_ensemble(F107, F107B,
+        # ensemble_EuvacIrr, ensemble_average_EuvacIrr, ensemble_stddev_EuvacIrr = irradiance_ensemble(F107, F107A,
         #                                                                                                 iterations=50,
         #                                                                                                 model='EUVAC')
-        euvacFlux, euvacIrr, perturbedEuvacIrr, savedPertsEuvac, cc2Euvac = euvac.euvac(F107, F107B,
+        euvacFlux, euvacIrr, perturbedEuvacIrr, savedPertsEuvac, cc2Euvac = euvac.euvac(F107, F107A,
                                                                                         statsFiles=None) #['corMatEUVAC.pkl',
                                                                                                     #'sigma_EUVAC.pkl'])
 
@@ -192,12 +208,19 @@ if __name__=="__main__":
 
         # Generate HEUVAC data:
         # ensemble_HeuvacIrr, ensemble_average_HeuvacIrr, ensemble_stddev_HeuvacIrr = irradiance_ensemble(F107,
-        #                                                                                              F107B,
+        #                                                                                              F107A,
         #                                                                                              iterations=50,
         #                                                                                              model='HEUVAC')
-        heuvac_wav, heuvacFlux, heuvacIrr, perturbedEuvIrradiance, savedPertsHeuvac, cc2Heuvac = heuvac.heuvac(F107, F107B,
-                                                                                                               torr=True,
-                                                                                                               statsFiles=None) #[
+        cachedData = toolbox.loadPickle(results_dir + 'cachedData.pkl')
+        heuvac_wav = cachedData["heuvac_wav"]
+        heuvacFlux = cachedData["heuvacFlux"]
+        heuvacIrr = cachedData["heuvacIrr"]
+        perturbedEuvIrradiance = cachedData["perturbedEuvIrradiance"]
+        savedPertsHeuvac = cachedData["savedPertsHeuvac"]
+        cc2Heuvac = cachedData["cc2Heuvac"]
+        # heuvac_wav, heuvacFlux, heuvacIrr, perturbedEuvIrradiance, savedPertsHeuvac, cc2Heuvac = heuvac.heuvac(F107, F107A,
+        #                                                                                                        torr=True,
+        #                                                                                                        statsFiles=None) #[
                                                                                                                    #'corMatHEUVAC.pkl',
                                                                                                                    #'sigma_HEUVAC.pkl'])
 
@@ -209,12 +232,12 @@ if __name__=="__main__":
                                                                                       tableFile=neuvac_tableFile_Stan_Bands,
                                                                                       statsFiles=['corMatStanBands.pkl',
                                                                                                   'sigma_NEUVAC_StanBands.pkl'])
-        solomonFluxHFG, solomonIrrHFG = solomon.solomon(F107, F107B, model='HFG')
-        solomonFluxEUVAC, solomonIrrEUVAC = solomon.solomon(F107, F107B, model='EUVAC')
 
-        #
-        solomonWavs, solomonIrrFISM2_test = toolbox.newbins(wavelengthsFISM2, myIrrDataAllFISM2, solomonTable,
-                        zero=False)
+        solomonFluxHFG, solomonIrrHFG = solomon.solomon(F107, F107A, model='HFG')
+        solomonFluxEUVAC, solomonIrrEUVAC = solomon.solomon(F107, F107A, model='EUVAC')
+        
+        # solomonWavs, solomonIrrFISM2_test = toolbox.newbins(wavelengthsFISM2, myIrrDataAllFISM2, solomonTable,
+        #                 zero=False)
         # plt.figure();
         # plt.plot(wavelengthsFISM2Bands, correspondingIrrFISM2StanBands[0, :], 'b-', label='FISM2-S')
         # plt.plot(wavelengthsFISM2Bands, correspondingIrrFISM2StanBands[-1, :], 'b--', label='FISM2-S (2)')
@@ -374,16 +397,10 @@ if __name__=="__main__":
     xPos = np.append(euvacTable[:, 1][bandInds], euvacTable[:, 2][-1]) # np.append(euvacTable[:, 1], euvacTable[:, 2][-1])
     sortInds = np.argsort(xPos)
     xPosSorted = xPos[sortInds]
-    def energy(watt):
-        """
-        Given the energy in watts, return the energy in joules, provided that the time given is in seconds.
-        """
-        return watt * 86400
-    def power(myEnergy):
-        return myEnergy / 86400
+
     fig, ax = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True)
     # i: Low Activity:
-    chosenDateLow = datetime(1985, 11, 4) # Beginning of Solar Cycle 21
+    chosenDateLow = datetime(2004, 3, 30) # 1985-11-04; Beginning of Solar Cycle 21
     idx, val = toolbox.find_nearest(times, chosenDateLow)
     ax[0].stairs(values=correspondingFism2Irr[idx, :][bandInds], edges=xPosSorted, label='FISM2', lw=9) # sortInds[:-1]
     # lowerBound = ensemble_average_NeuvacIrr[idx, :][bandInds] - ensemble_stddev_NeuvacIrr[idx, :][bandInds]
@@ -402,9 +419,8 @@ if __name__=="__main__":
     secax.set_ylabel('Radiant Exposure (J/m$^2$)', rotation=-90, labelpad=15)
     ax[0].set_title('Solar Spectra during Low Solar Activity ('+str(chosenDateLow)[:-9]+')')
     # ii: High Activity:
-    chosenDateHigh = datetime(1991, 1, 31) # Peak of Solar Cycle 21
+    chosenDateHigh = datetime(2002, 2, 8) # 1991-01-31; Peak of Solar Cycle 21
     idx, val = toolbox.find_nearest(times, chosenDateHigh)
-    chosenDateLow = datetime(1985, 11, 4)  # Beginning of Solar Cycle 21
     ax[1].stairs(values=correspondingFism2Irr[idx, :][bandInds], edges=xPosSorted, label='FISM2', lw=9)
     ax[1].stairs(values=ensemble_average_NeuvacIrr[idx, :][bandInds], edges=xPosSorted, label='NEUVAC-37 (n='+str(iterations)+')', lw=3)
     ax[1].errorbar(xPosSorted[:-1] + 25, ensemble_average_NeuvacIrr[idx, :][bandInds],
@@ -584,8 +600,8 @@ if __name__=="__main__":
     # bins = np.linspace(np.nanmin(NEUVAC_resids_flat), np.nanmax(NEUVAC_resids_flat), num=100)
     bins = np.linspace(-25., 25., num=100)
 
-    myLabels = ['Percent Deviation (%)', 'Count', 'NEUVAC Percent Deviation from FISM2']
-    figHist = toolbox.plotHist(NEUVAC_resids_flat, bins=bins, color='orange', saveLoc=results_dir + 'NEUVAC_percDev.png', labels=myLabels, density=False)
+    # myLabels = ['Percent Deviation (%)', 'Count', 'NEUVAC Percent Deviation from FISM2']
+    # figHist = toolbox.plotHist(NEUVAC_resids_flat, bins=bins, color='orange', saveLoc=results_dir + 'NEUVAC_percDev.png', labels=myLabels, density=False)
 
     # Percent Deviation for just three bins: 425 A (12), 725 A (24), and 975 A (31)
     NEUVAC_resids_425 = toolbox.percDev(ensemble_average_NeuvacIrr[:, 12], correspondingFism2Irr[:, 12])
@@ -778,7 +794,7 @@ if __name__=="__main__":
     xPosSortedSolomon = xPosSolomon[sortIndsSolomon]
     fig, ax = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True)
     # i: Low Activity:
-    chosenDateLow = datetime(1985, 11, 4)  # Beginning of Solar Cycle 21
+    chosenDateLow = datetime(2004, 3, 30)  # 1985-11-04 Beginning of Solar Cycle 21
     idx, val = toolbox.find_nearest(times, chosenDateLow)
     ax[0].stairs(values=FISM2S_sums[idx, :][sortIndsSolomon[:-1]], edges=xPosSortedSolomon, label='FISM2', lw=9)
     ax[0].errorbar(xPosSortedSolomon[:-1]+halfBandWidths[goodInds], NEUVAC_22_sums[idx, :],
@@ -797,9 +813,8 @@ if __name__=="__main__":
     ax[0].set_title('Solar Spectra during Low Solar Activity (' + str(chosenDateLow)[:-9] + ')')
     ax[0].set_ylim([1e-6, 1e-2])
     # ii: High Activity:
-    chosenDateHigh = datetime(1991, 1, 31)  # Peak of Solar Cycle 21
+    chosenDateHigh = datetime(2002, 2, 8)  # 1991-01-31 Peak of Solar Cycle 21
     idx, val = toolbox.find_nearest(times, chosenDateHigh)
-    chosenDateLow = datetime(1985, 11, 4)  # Beginning of Solar Cycle 21
     ax[1].stairs(values=FISM2S_sums[idx, :][sortIndsSolomon[:-1]], edges=xPosSortedSolomon, label='FISM2', lw=9)
     ax[1].errorbar(xPosSortedSolomon[:-1]+halfBandWidths[goodInds], NEUVAC_22_sums[idx, :],
                    yerr=NEUVAC_22_sttdev[idx, :], capsize=7, capthick=2, fmt='o', color='orange')
@@ -962,8 +977,8 @@ if __name__=="__main__":
     #                    np.max([np.nanpercentile(NEUVAC_resids, 75), np.nanpercentile(EUVAC_resids, 75), np.nanpercentile(HEUVAC_resids, 75)]), num=100)
     # bins = np.linspace(np.nanmin(NEUVAC_resids_flat_Solomon), np.nanmax(NEUVAC_resids_flat_Solomon), num=100)
     bins = np.linspace(-25., 25., num=100)
-    myLabels = ['Percent Deviation (%)', 'Count', 'NEUVAC Percent Deviation from FISM2']
-    figHist = toolbox.plotHist(NEUVAC_resids_flat_Solomon, bins=bins, color='orange', saveLoc=results_dir + 'NEUVAC_percDev_SOLOMON.png', labels=myLabels, density=False)
+    # myLabels = ['Percent Deviation (%)', 'Count', 'NEUVAC Percent Deviation from FISM2']
+    # figHist = toolbox.plotHist(NEUVAC_resids_flat_Solomon, bins=bins, color='orange', saveLoc=results_dir + 'NEUVAC_percDev_SOLOMON.png', labels=myLabels, density=False)
 
     # Percent Deviation for just three bins: 430 A (9), 724 A (11), and 981 A (19)
     NEUVAC_resids_430 = toolbox.percDev(ensemble_average_NeuvacIrrSolomon[:, 9], correspondingIrrFISM2StanBands[:, 9])

experiments/fitNeuvac.py
@@ -64,6 +64,7 @@ if __name__=="__main__":
     euv_data_59 = read_euv_csv_file(euv_folder + 'euv_59.csv', band=False)
     mids = 0.5 * (euv_data_59['long'] + euv_data_59['short'])
 
+    refit = [True, True] # Control whether not refitting is done for the GITM bins and the Solomon bins, respectively.
     # ==================================================================================================================
     # GITM BINS
     # ==================================================================================================================
@@ -79,42 +80,43 @@ if __name__=="__main__":
     myIrrDataAllFISM2Fixed[myIrrDataAllFISM2Fixed <= 0 ] = np.nan
     # FISM2 data extends between 1947-02-14; 00:00 and 2023-08-29; 00:00.
 
-    # Perform a non-linear fit between F10.7, F10.7B, and FISM2:
-    neuvacTable = neuvac.neuvacFit([times, F107, F107B], myIrrTimesFISM2, myIrrDataAllFISM2Fixed, wavelengths=mids, label='FISM2')
+    if refit[0] == True:
+        # Perform a non-linear fit between F10.7, F10.7B, and FISM2:
+        neuvacTable = neuvac.neuvacFit([times, F107, F107B], myIrrTimesFISM2, myIrrDataAllFISM2Fixed, wavelengths=mids, label='FISM2')
 
-    # Print the coefficients to a file:
-    with open(neuvac_directory+'neuvac_table.txt', 'w') as output:
-        # Write the header information:
-        output.write('This file contains coefficients for the current iteration of NEUVAC.\n'
-                     'NOTE THAT THIS FILE CONTAINS COEFFICIENTS FOR THE GITM BINS.\n'
-                     'This file was created on '+datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S')+'\n'
-                     'File Authors: Brandt, Daniel A. and Ridley, Aaron J.\n'
-                     'This version of NEUVAC was created by fitting nonlinear models between F10.7 and preceding\n'
-                     '54-day averaged F10.7 and FISM2 decomposed into 59 wavelength bands conventionally used in\n'
-                     'the GITM and Aether models.\n'
-                     'The file is formatted as follows:\n'
-                     ' - First Column: Lower limit of the given wavelength bin in Angstroms.\n'
-                     ' - Second Column: Upper limit of the given wavelength bin in Angstroms.\n'
-                     ' - Third through Eighth colummns: Coefficients for the model.\n'
-                     'The functional form of the model is given by:\n'
-                     'Irr_i(t) = A_i * (F107(t) ** B_i) + C_i * (F107B(t) ** D_i) + E_i * (F107B(t) - F107(t)) + F_i\n'
-                     'where the irradiance in bin i (Irr_i) is a function of time t, and A_i through F_i are \n'
-                     'coefficients for bin i, and F107(t) and F107B(t) represent values of the F10.7 and 54-day\n'
-                     'averaged F10.7 computed with a backwards-looking window, respectively.\n'
-                     '-----------------------------------------------------------------------------------------------\n'
-                     'WAVES WAVEL A_i B_i C_i D_i E_i F_i\n')
-        for i in range(neuvacTable.shape[0]):
-            output.writelines(str(euv_data_59['short'][i])+' '+str(euv_data_59['long'][i])+' '+toolbox.stringList(neuvacTable[i, :])+'\n')
+        # Print the coefficients to a file:
+        with open(neuvac_directory+'neuvac_table.txt', 'w') as output:
+            # Write the header information:
+            output.write('This file contains coefficients for the current iteration of NEUVAC.\n'
+                         'NOTE THAT THIS FILE CONTAINS COEFFICIENTS FOR THE GITM BINS.\n'
+                         'This file was created on '+datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S')+'\n'
+                         'File Authors: Brandt, Daniel A. and Ridley, Aaron J.\n'
+                         'This version of NEUVAC was created by fitting nonlinear models between F10.7 and preceding\n'
+                         '54-day averaged F10.7 and FISM2 decomposed into 59 wavelength bands conventionally used in\n'
+                         'the GITM and Aether models.\n'
+                         'The file is formatted as follows:\n'
+                         ' - First Column: Lower limit of the given wavelength bin in Angstroms.\n'
+                         ' - Second Column: Upper limit of the given wavelength bin in Angstroms.\n'
+                         ' - Third through Eighth colummns: Coefficients for the model.\n'
+                         'The functional form of the model is given by:\n'
+                         'Irr_i(t) = A_i * (F107(t) ** B_i) + C_i * (F107B(t) ** D_i) + E_i * (F107B(t) - F107(t)) + F_i\n'
+                         'where the irradiance in bin i (Irr_i) is a function of time t, and A_i through F_i are \n'
+                         'coefficients for bin i, and F107(t) and F107B(t) represent values of the F10.7 and 54-day\n'
+                         'averaged F10.7 computed with a backwards-looking window, respectively.\n'
+                         '-----------------------------------------------------------------------------------------------\n'
+                         'WAVES WAVEL A_i B_i C_i D_i E_i F_i\n')
+            for i in range(neuvacTable.shape[0]):
+                output.writelines(str(euv_data_59['short'][i])+' '+str(euv_data_59['long'][i])+' '+toolbox.stringList(neuvacTable[i, :])+'\n')
 
-    neuvacIrr, _, _, _ = neuvac.neuvacEUV(F107, F107B, bands=None, tableFile=neuvac_tableFile)
+        neuvacIrr, _, _, _ = neuvac.neuvacEUV(F107, F107B, bands=None, tableFile=neuvac_tableFile)
 
-    # View the result of the model fits, as a sanity check:
-    # for i in range(neuvacIrr.shape[1]):
-    #     plt.figure()
-    #     plt.plot(myIrrTimesFISM2, myIrrDataAllFISM2Fixed[:, i], label='FISM2')
-    #     plt.plot(times, neuvacIrr[:, i], label='NEUVAC')
-    #     plt.legend(loc='best')
-    #     plt.title('Irradiance Time Series at :'+str(np.round(myIrrDataWavelengthsFISM2[i],2))+' Angstroms')
+        # View the result of the model fits, as a sanity check:
+        # for i in range(neuvacIrr.shape[1]):
+        #     plt.figure()
+        #     plt.plot(myIrrTimesFISM2, myIrrDataAllFISM2Fixed[:, i], label='FISM2')
+        #     plt.plot(times, neuvacIrr[:, i], label='NEUVAC')
+        #     plt.legend(loc='best')
+        #     plt.title('Irradiance Time Series at :'+str(np.round(myIrrDataWavelengthsFISM2[i],2))+' Angstroms')
 
     # ==================================================================================================================
     # STANDARD BANDS
@@ -130,43 +132,51 @@ if __name__=="__main__":
     myIrrDataAllFISM2BandsFixed = myIrrDataAllFISM2Bands.copy()
     myIrrDataAllFISM2BandsFixed[myIrrDataAllFISM2BandsFixed <= 0] = np.nan
 
-    # Perform a non-linear fit between F10.7, F10.7B, and FISM2:
-    midsS = (wavelengthsFISM2Bands * 10)[:-1]
-    neuvacTableS = neuvac.neuvacFit([times, F107, F107B], myIrrTimesFISM2Bands, myIrrDataAllFISM2BandsFixed[:, :-1], wavelengths=midsS,
-                                   label='FISM2S')
+    # Rebin the high-resolution daily FISM2 data into the Solomon bins, as a sanity check:
+    matchInds = np.where((myIrrTimesFISM2Bands >= myIrrTimesFISM2[0]) & (myIrrTimesFISM2Bands <= myIrrTimesFISM2[-1]))[0]
+    matchedFISM2IrrSolomon = myIrrDataAllFISM2BandsFixed[matchInds, :]
+    myWavelengthsFISM2Bands, rebinnedIrrDataFISM2Bands = toolbox.solomonAnalysis(matchedFISM2IrrSolomon, solomonTable,
+                                                                                 myIrrDataAllFISM2,
+                                                                                 wavelengthsFISM2)  # .newbins(wavelengthsFISM2, myIrrDataAllFISM2, solomonTable, zero=True)
 
-    # Print the coefficients to a file:
-    with open(neuvac_directory+'neuvac_table_stan_bands.txt', 'w') as output:
-        # Write the header information:
-        output.write('This file contains coefficients for the current iteration of NEUVAC.\n'
-                     'NOTE THAT THIS FILE CONTAINS COEFFICIENTS FOR THE STAN BANDS.\n'
-                     'This file was created on '+datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S')+'\n'
-                     'File Authors: Brandt, Daniel A. and Ridley, Aaron J.\n'
-                     'This version of NEUVAC was created by fitting nonlinear models between F10.7 and preceding\n'
-                     '54-day averaged F10.7 and FISM2 decomposed into 23 wavelength bands conventionally used in\n'
-                     'numerous atmospheric models, such as SAMI3.\n'
-                     'The file is formatted as follows:\n'
-                     ' - First Column: Lower limit of the given wavelength bin in Angstroms.\n'
-                     ' - Second Column: Upper limit of the given wavelength bin in Angstroms.\n'
-                     ' - Third through Eighth colummns: Coefficients for the model.\n'
-                     'The functional form of the model is given by:\n'
-                     'Irr_i(t) = A_i * (F107(t) ** B_i) + C_i * (F107B(t) ** D_i) + E_i * (F107B(t) - F107(t)) + F_i\n'
-                     'where the irradiance in bin i (Irr_i) is a function of time t, and A_i through F_i are \n'
-                     'coefficients for bin i, and F107(t) and F107B(t) represent values of the F10.7 and 54-day\n'
-                     'averaged F10.7 computed with a backwards-looking window, respectively.\n'
-                     '-----------------------------------------------------------------------------------------------\n'
-                     'WAVES WAVEL A_i B_i C_i D_i E_i F_i\n')
-        for i in range(neuvacTableS.shape[0]):
-            output.writelines(str(solomonTable['short'][i])+' '+str(solomonTable['long'][i])+' '+toolbox.stringList(neuvacTableS[i, :])+'\n')
+    if refit[1] == True:
+        # Perform a non-linear fit between F10.7, F10.7B, and FISM2:
+        midsS = (wavelengthsFISM2Bands * 10)[:-1]
+        neuvacTableS = neuvac.neuvacFit([times, F107, F107B], myIrrTimesFISM2Bands, myIrrDataAllFISM2BandsFixed[:, :-1], wavelengths=midsS,
+                                       label='FISM2S')
 
-    neuvacIrrS, _, _, _ = neuvac.neuvacEUV(F107, F107B, bands='SOLOMON', tableFile=neuvac_tableFile_Stan_Bands)
-    # View the result of the model fits, as a sanity check:
-    # for i in range(neuvacIrrS.shape[1]):
-    #     plt.figure()
-    #     plt.plot(myIrrTimesFISM2Bands, myIrrDataAllFISM2BandsFixed[:, i], label='FISM2S')
-    #     plt.plot(times, neuvacIrrS[:, i], label='NEUVAC')
-    #     plt.legend(loc='best')
-    #     plt.title('Irradiance Time Series at :'+str(np.round(midsS[i],2))+' Angstroms')
+        # Print the coefficients to a file:
+        with open(neuvac_directory+'neuvac_table_stan_bands.txt', 'w') as output:
+            # Write the header information:
+            output.write('This file contains coefficients for the current iteration of NEUVAC.\n'
+                         'NOTE THAT THIS FILE CONTAINS COEFFICIENTS FOR THE STAN BANDS.\n'
+                         'This file was created on '+datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S')+'\n'
+                         'File Authors: Brandt, Daniel A. and Ridley, Aaron J.\n'
+                         'This version of NEUVAC was created by fitting nonlinear models between F10.7 and preceding\n'
+                         '54-day averaged F10.7 and FISM2 decomposed into 23 wavelength bands conventionally used in\n'
+                         'numerous atmospheric models, such as SAMI3.\n'
+                         'The file is formatted as follows:\n'
+                         ' - First Column: Lower limit of the given wavelength bin in Angstroms.\n'
+                         ' - Second Column: Upper limit of the given wavelength bin in Angstroms.\n'
+                         ' - Third through Eighth colummns: Coefficients for the model.\n'
+                         'The functional form of the model is given by:\n'
+                         'Irr_i(t) = A_i * (F107(t) ** B_i) + C_i * (F107B(t) ** D_i) + E_i * (F107B(t) - F107(t)) + F_i\n'
+                         'where the irradiance in bin i (Irr_i) is a function of time t, and A_i through F_i are \n'
+                         'coefficients for bin i, and F107(t) and F107B(t) represent values of the F10.7 and 54-day\n'
+                         'averaged F10.7 computed with a backwards-looking window, respectively.\n'
+                         '-----------------------------------------------------------------------------------------------\n'
+                         'WAVES WAVEL A_i B_i C_i D_i E_i F_i\n')
+            for i in range(neuvacTableS.shape[0]):
+                output.writelines(str(solomonTable['short'][i])+' '+str(solomonTable['long'][i])+' '+toolbox.stringList(neuvacTableS[i, :])+'\n')
+
+        neuvacIrrS, _, _, _ = neuvac.neuvacEUV(F107, F107B, bands='SOLOMON', tableFile=neuvac_tableFile_Stan_Bands)
+        # View the result of the model fits, as a sanity check:
+        # for i in range(neuvacIrrS.shape[1]):
+        #     plt.figure()
+        #     plt.plot(myIrrTimesFISM2Bands, myIrrDataAllFISM2BandsFixed[:, i], label='FISM2S')
+        #     plt.plot(times, neuvacIrrS[:, i], label='NEUVAC')
+        #     plt.legend(loc='best')
+        #     plt.title('Irradiance Time Series at :'+str(np.round(midsS[i],2))+' Angstroms')
 
     print('NEUVAC model fitting complete.')
 #-----------------------------------------------------------------------------------------------------------------------

experiments/uncIrradiance.py
@@ -257,24 +257,24 @@ if __name__=="__main__":
     plt.savefig('Uncertainty/corMatsEUVAC.png', dpi=300)
 
     # 6b: HEUVAC
-    heuvac_wav, heuvacFlux, heuvacIrr, _, _, _ = heuvac.heuvac(F107, F107B, torr=True, statsFiles=['corMatHEUVAC.pkl',
-                                                                                           'sigma_HEUVAC.pkl'])
-    residualsArrayHEUVAC = np.subtract(heuvacIrr, correspondingIrrFISM2[:, 7:44])
-    toolbox.savePickle(residualsArrayHEUVAC, 'residualsArrayHEUVAC.pkl')
-    corMatHEUVAC = toolbox.mycorrelate2d(residualsArrayHEUVAC, normalized=True)
-    toolbox.savePickle(corMatHEUVAC, 'corMatHEUVAC.pkl')
-    STDNeuvacResidsHEUVAC = np.zeros(heuvacIrr.shape[1])
-    for i in range(STDNeuvacResidsHEUVAC.shape[0]):
-        STDNeuvacResidsHEUVAC[i] = np.nanstd(residualsArrayHEUVAC[:, i])
-    toolbox.savePickle(STDNeuvacResidsHEUVAC, 'sigma_HEUVAC.pkl')
-    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
-    pos = ax.imshow(corMatHEUVAC, aspect='auto', cmap='bwr', vmin=0.9999, vmax=1., interpolation='none')
-    ax.set_xlabel('Wavelength Band', fontsize=16)
-    ax.set_ylabel('Wavelength Band', fontsize=16)
-    fig.suptitle('Correlation Matrix (HEUVAC - FISM2)', fontsize=18)
-    plt.colorbar(pos)
-    fig.subplots_adjust(top=0.9)
-    plt.savefig('Uncertainty/corMatsHEUVAC.png', dpi=300)
+    # heuvac_wav, heuvacFlux, heuvacIrr, _, _, _ = heuvac.heuvac(F107, F107B, torr=True, statsFiles=['corMatHEUVAC.pkl',
+    #                                                                                        'sigma_HEUVAC.pkl'])
+    # residualsArrayHEUVAC = np.subtract(heuvacIrr, correspondingIrrFISM2[:, 7:44])
+    # toolbox.savePickle(residualsArrayHEUVAC, 'residualsArrayHEUVAC.pkl')
+    # corMatHEUVAC = toolbox.mycorrelate2d(residualsArrayHEUVAC, normalized=True)
+    # toolbox.savePickle(corMatHEUVAC, 'corMatHEUVAC.pkl')
+    # STDNeuvacResidsHEUVAC = np.zeros(heuvacIrr.shape[1])
+    # for i in range(STDNeuvacResidsHEUVAC.shape[0]):
+    #     STDNeuvacResidsHEUVAC[i] = np.nanstd(residualsArrayHEUVAC[:, i])
+    # toolbox.savePickle(STDNeuvacResidsHEUVAC, 'sigma_HEUVAC.pkl')
+    # fig, ax = plt.subplots(1, 1, figsize=(10, 8))
+    # pos = ax.imshow(corMatHEUVAC, aspect='auto', cmap='bwr', vmin=0.9999, vmax=1., interpolation='none')
+    # ax.set_xlabel('Wavelength Band', fontsize=16)
+    # ax.set_ylabel('Wavelength Band', fontsize=16)
+    # fig.suptitle('Correlation Matrix (HEUVAC - FISM2)', fontsize=18)
+    # plt.colorbar(pos)
+    # fig.subplots_adjust(top=0.9)
+    # plt.savefig('Uncertainty/corMatsHEUVAC.png', dpi=300)
 
     # 6c: TODO: SOLOMON (HFG and EUVAC) -- Optional
 

tools/processIrradiances.py
@@ -3,6 +3,8 @@
 #-----------------------------------------------------------------------------------------------------------------------
 # Top-level Imports
 import os, sys
+
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 import numpy as np
 import pickle
@@ -189,7 +191,7 @@ def obtainFism2(myFism2File, bands=False):
     wavelengths = np.asarray(fism2Data.variables['wavelength'])
     if bands==True: # STANDARD BANDS
         flux = np.asarray(fism2Data.variables['ssi']) # photons/cm2/second
-        bandwidths = np.asarray(fism2Data.variables['band_width'])
+        # bandwidths = np.asarray(fism2Data.variables['band_width'])
         pFlux = flux * 1.0e4 # photons/m2/second
         # Convert fluxes to irradiances:
         irr = np.zeros_like(flux)
@@ -380,3 +382,35 @@ def obtainSDO(seeFile):
     #
     # sys.exit(0)
 # -----------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    datetimes, wavelengths, irradiances, uncertainties = obtainSEE(TIMED_spectra_folder+'latest_see_L3_merged_2002-2023.ncdf')
+    datetimesF, wavelengthsF, irradiancesF, uncertaintiesF = obtainFism2(fism2_spectra_folder+'daily_data_1947-2023.nc')
+
+    import matplotlib.pylab as pylab
+    params = {'legend.fontsize': 'x-large',
+              'figure.figsize': (14, 6),
+              'axes.labelsize': 'x-large',
+              'axes.titlesize': 'X-Large',
+              'xtick.labelsize': 'X-large',
+              'ytick.labelsize': 'X-large'}
+    sampleVals1 = irradiancesF[21056, :]
+    idx = np.flatnonzero(~np.isnan(sampleVals1))
+    sampleVals1[idx[sampleVals1[idx] < 0]] = np.nan
+    sampleVals2 = irradiances[1000, :]
+    idx = np.flatnonzero(~np.isnan(sampleVals2))
+    sampleVals2[idx[sampleVals2[idx] < 0]] = np.nan
+    pylab.rcParams.update(params)
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.plot(wavelengthsF*10, sampleVals1, color='b', label='FISM2')
+    plt.plot(wavelengths*10, sampleVals2, color='r', label='TIMED/SEE')
+    plt.xlabel('Wavelength (Angstroms)')
+    plt.ylabel('Irradiance (W/m$^2$/nm)')
+    plt.title('Solar EUV Spectra on '+str(datetimes[1000].date()))
+    plt.legend(loc='best')
+    plt.yscale('log')
+
+    # TODO: Fix plot axis labels (fontsize), add a title, etc.
+
+    ellipsis

tools/toolbox.py
@@ -22,6 +22,7 @@ from urllib.request import urlretrieve
 from math import log10, floor
 from scipy import stats
 import seaborn as sns
+import scipy.optimize as opt
 #-----------------------------------------------------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -841,7 +842,7 @@ def rebin(wavelengths, data, resolution, limits=None, factor=None, zero=True, un
 
     return newWaves, newData
 
-def newbins(wavelengths, data, bins, zero=False):
+def newbins(wavelengths, data, bins, zero=False, interpolation=False):
     """
     Rebin data according to a user-defined binning scheme.
     :param wavelengths: arraylike
@@ -853,6 +854,9 @@ def newbins(wavelengths, data, bins, zero=False):
         (right key). Units should be in Angstroms.
     :param zero: bool
         Controls whether a wavelength bin is 'zeroed' out after it is considered. Default is False.
+    :param interpolation: bool
+        Controls whether or not interpolation or 4-nm centered windowed averaging is used to handle singular emission
+        lines. Default is False, corresponding to the windowed averaging.
     :return newWaves: arraylike
         The wavelength boundaries of the new binning scheme.
     :return newData: arraylike
@@ -865,13 +869,13 @@ def newbins(wavelengths, data, bins, zero=False):
     newData = np.zeros((data.shape[0], len(lowBins)))
     newWaves = 0.5*(lowBins + highBins)
     # Relevant line indices in FISM2:
-    fismLines = np.array([257, 283, 303, 304, 367, 464, 553, 583, 610, 630, 702, 764, 769, 787, 976, 1026, 1032, 1216])
+    fismLines = np.array([256, 284, 303, 304, 368, 465, 554, 584, 609, 629, 703, 765, 770, 787, 976, 1025, 1031, 1215])
 
     # Loop through the desired wavelengths:
     halfWindow = 2
     singulars = []
     j = 0
-    for i in range(len(lowBins)):
+    for i in range(len(lowBins)): # 32
         # Go through the singular wavelengths first.
         if lowBins[i] == highBins[i]:
             # idx, val = find_nearest(wavelengths, lowBins[i] / 10.)
@@ -879,15 +883,43 @@ def newbins(wavelengths, data, bins, zero=False):
             # Simply assign the the values at that index to the new bin values:
             # newData[:, i] = data[:, fismLines[j]] * nativeBinWidth
 
-            # Sum the data in a 5-nm window centered on the line:
-            newData[:, i] = np.sum(data[:, fismLines[j]-halfWindow:fismLines[j]+halfWindow] * nativeBinWidth, axis=-1 )
+            if interpolation == False:
+                # Sum the data in a 0.4-nm window centered on the line:
+                newData[:, i] = np.sum(data[:, fismLines[j]-halfWindow:fismLines[j]+halfWindow] * nativeBinWidth, axis=-1 )
+            else:
+                # Aaronic method:
+                # 1: Linearly-interpolate between the adjacent bins to approximate the background value
+                interpolatedValues = np.zeros_like(data[:, 0])
+                x = [wavelengths[fismLines[j]-1], wavelengths[fismLines[j]+1]]
+                for k in range(data[:, fismLines[j]].shape[0]):
+                    y = [data[k, fismLines[j]-1], data[k, fismLines[j]+1]]
+                    interpVal = np.interp(wavelengths[fismLines[j]], x, y)
+                    interpolatedValues[k] = interpVal
+
+                # 2: Take the difference between the native FISM2 bin value and interpolated value
+                difference = data[:, fismLines[j]] - interpolatedValues
+
+                # 3: Assign the difference as the line irradiance (increase in brightness above background)
+                newData[:, i] = difference * nativeBinWidth
+
+                # 4: Sanity check - view the results:
+                idx = k
+                plt.figure()
+                plt.plot(wavelengths, data[idx, :])
+                plt.axvline(x=wavelengths[fismLines[j]], color='k')
+                plt.scatter(x=wavelengths[fismLines[j]], y=data[idx, fismLines[j]], color='b')
+                plt.scatter(x=wavelengths[fismLines[j]], y=interpolatedValues[idx], color='r')
+                plt.scatter(x=wavelengths[fismLines[j]], y=difference[idx], color='m')
 
             singulars.append(lowBins[i])
 
             # Performing zeroing, if desired:
             if zero == True:
                 # data[:, fismLines[j]] = np.zeros_like(data[:, fismLines[j]])
-                data[:, fismLines[j] - halfWindow:fismLines[j] + halfWindow] = np.zeros_like(data[:, fismLines[j]-halfWindow:fismLines[j]+halfWindow])
+                if interpolation == False:
+                    data[:, fismLines[j] - halfWindow:fismLines[j] + halfWindow] = np.zeros_like(data[:, fismLines[j]-halfWindow:fismLines[j]+halfWindow])
+                else:
+                    data[:, fismLines[j]-1:fismLines[j] + 1] = np.zeros_like(data[:, fismLines[j]-1:fismLines[j] + 1])
             j += 1
 
     # plt.figure(); plt.plot(wavelengths*10, originalData[0, :], 'bo-'); plt.plot(wavelengths*10, data[0, :], 'ro-')
@@ -1164,4 +1196,81 @@ def percDev(x, y):
         The true value(s).
     """
     return np.divide(np.subtract(x, y), y) * 100
+
+def solomonAnalysis(fismStanBands, fismWavesSolomon, fismDaily, fismWavesDaily):
+    """
+    Given FISM2 data in the standard bands and in the high-resolution wavelength scheme for the daily values, figure
+    out what coefficients are needed to rebin the daily values into the standard bands.
+    :param fismStanBands: ndarray
+        A 2D array of FISM2 data in the standard bands from Solomon and Qian. Each row is an observation and each column
+        a wavelength bin.
+    :param fismWavesSolomon: dict
+        The wavelength boundaries for the Solomon binning scheme. fismWavesSolomon['short'] contains the left-sided
+        bin boundaries, and fismWavesSolomon['long'] contains the right-sided bin boundaries. Units in Angstroms.
+    :param fismDaily: ndarray
+        A 2D array of FISM2 data in the high-resolution wavelength scheme for the daily spectra. Each row is an
+        observation and each column a wavelength bin.
+    :param fismWavesDaily: ndarray
+        The wavelengths at which every FISM2 daily value is reported. Units in nm.
+    """
+    nativeBinWidth = np.round(np.nanmean(np.diff(fismWavesDaily)), 2)
+    manualFismStanBands = np.zeros_like(fismStanBands)
+    fismStanBandsShort = fismWavesSolomon['short'] / 10.
+    fismStanBandsLong = fismWavesSolomon['long'] / 10.
+
+    # Define an optimization function:
+    def f(x):
+        y = np.dot(A, x) - b
+        return np.dot(y, y)
+
+    # Loop through the solomon bins:
+    for i in range(len(fismWavesSolomon['short'])):
+        # For the case where the bins ARE NOT overlapping:
+        validInds = np.where((fismWavesDaily >= fismStanBandsShort[i]) & (fismWavesDaily < fismStanBandsLong[i]))[0]
+        # Sum the values in the bins:
+        manualFismStanBands[:, i] = np.sum(fismDaily[:, validInds] * nativeBinWidth, axis=-1)
+
+    # Handle the overlapping bins individually:
+    overlappingSections = [[11, 13], [13, 16], [16, 19]]
+    coeffs = []
+    for pair in overlappingSections:
+        validIndsOverlapping = np.where((fismWavesDaily >= fismStanBandsShort[pair[0]]) & (fismWavesDaily <= fismStanBandsLong[pair[0]]))[0]
+        totalIrradianceInSection = np.sum(fismDaily[:, validIndsOverlapping], axis=-1)* 0.1 # W/m^2
+        referenceIrradiance = fismStanBands[:, pair[0]:pair[1]] # W/m^2
+        # Solve the linear system:
+        currentCoeffs = []
+        for j in range(len(totalIrradianceInSection)):
+            # Set up the system of equations:
+            if referenceIrradiance.shape[1] > 2:
+                A = np.array([
+                    [totalIrradianceInSection[j], 0, 0],
+                    [0, totalIrradianceInSection[j], 0],
+                    [0, 0, totalIrradianceInSection[j]],
+                    ])
+                b = np.array([
+                    referenceIrradiance[j, 0],
+                    referenceIrradiance[j, 1],
+                    referenceIrradiance[j, 2],
+                    ])
+                cons = ({'type': 'eq', 'fun': lambda x: x.sum() - 1})
+                res = opt.minimize(f, [0.2, 0.3, 0.5], method='SLSQP', constraints=cons, options={'disp': False})
+                x = res['x']
+            else:
+                A = np.array([
+                    [],
+                    [1, 1, 1]
+                ])
+                b = np.array([
+                    0,
+                    1
+                ])
+            x = np.linalg.solve(A, b)
+            currentCoeffs.append(x)
+
+
+
+
+    ellipsis
+
+
 #-----------------------------------------------------------------------------------------------------------------------